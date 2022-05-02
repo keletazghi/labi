32497875d97a91c941a3338884e67ca97245c751
keletazghi <tesfazghikulut@gmail.com>
2022-05-01 22:44:51 -0500

    ok

---
+++ lab1/labscript/scrip.js
@@ -0,0 +1,12 @@
+function addTask(){ 
+            
+    var s= document.getElementById("text");
+    
+    document.getElementById("add").innerText=s.value;
+    
+    
+    
+ }
+ function clearTask(){
+     document.getElementById("add").innerHTML="";
+ }
