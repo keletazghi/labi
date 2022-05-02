5dd5d8ba5c8e1a81fa1c9f1467f9aba784186298
keletazghi <tesfazghikulut@gmail.com>
2022-05-01 23:14:44 -0500

    task5

--- lab1/labscript/scrip.js
+++ lab1/labscript/scrip.js
@@ -1,11 +1,8 @@
-function addTask(){ 
-            
-    var s= document.getElementById("text");
-    
-    document.getElementById("add").innerText=s.value;
-    
-    
-    
+function addTask(){  
+    let textarea = document.getElementById("add").value;           
+    let s= document.getElementById("text");    
+    document.getElementById("add").innerText=textarea+ "\n "+s.value; 
+    s.value = '';   
  }
  function clearTask(){
      document.getElementById("add").innerHTML="";
